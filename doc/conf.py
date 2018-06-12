fb35682567b563e4a5a2330eb1cdd447c97d8c69
weikang9009 <weikang9009@gmail.com>
2018-06-12 15:30:37 -0700

    fix equations - sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -18,8 +18,11 @@
 #
 import os
 import sys
+import sphinx_bootstrap_theme
+
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
+os.path.abspath("../giddy/")
 
 
 # -- General configuration ------------------------------------------------
@@ -30,7 +33,16 @@ sys.path.insert(0, os.path.abspath('../..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_gallery.gen_gallery', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx_gallery.gen_gallery',
+              'sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.viewcode',
+              'sphinxcontrib.bibtex',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.doctest',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.napoleon']
 
 sphinx_gallery_conf = {
      # path to your examples scripts
@@ -90,18 +102,103 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
+#html_theme = 'bootstrap'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_title = "giddy documentation website"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {
+#
+#     # Navigation bar title. (Default: ``project`` value)
+#     'navbar_title': "giddy",
+#
+#     # Tab name for entire site. (Default: "Site")
+#     'navbar_site_name': "Site",
+#
+#     # A list of tuples containing pages or urls to link to.
+#     # Valid tuples should be in the following forms:
+#     #    (name, page)                 # a link to a page
+#     #    (name, "/aa/bb", 1)          # a link to an arbitrary relative url
+#     #    (name, "http://example.com", True) # arbitrary absolute url
+#     # Note the "1" or "True" value above as the third argument to indicate
+#     # an arbitrary url.
+#     # 'navbar_links': [
+#     #      ("Center for Geospatial Sciences is launching soon!", "news"),
+#     #    ("About", "about"),
+#     #    ("Projects", "projects"),
+#     #    ("Publications", "publications"),
+#     #    ("Education", "education"),
+#     #    ("Software", "software"),
+#     #    ("News", "news"),
+#     # ],
+#
+#     # Render the next and previous page links in navbar. (Default: true)
+#     'navbar_sidebarrel': True,
+#     #'navbar_sidebarrel': False,
+#
+#     # Render the current pages TOC in the navbar. (Default: true)
+#     #'navbar_pagenav': True,
+#     #'navbar_pagenav': False,
+#
+#     # No sidebar
+#     'nosidebar': False,
+#
+#     # Tab name for the current pages TOC. (Default: "Page")
+#     #'navbar_pagenav_name': "Page",
+#
+#     # Global TOC depth for "site" navbar tab. (Default: 1)
+#     # Switching to -1 shows all levels.
+#     #'globaltoc_depth': 2,
+#
+#     # Include hidden TOCs in Site navbar?
+#     #
+#     # Note: If this is "false", you cannot have mixed ``:hidden:`` and
+#     # non-hidden ``toctree`` directives in the same page, or else the build
+#     # will break.
+#     #
+#     # Values: "true" (default) or "false"
+#     #'globaltoc_includehidden': "false",
+#
+#     # HTML navbar class (Default: "navbar") to attach to <div> element.
+#     # For black navbar, do "navbar navbar-inverse"
+#     #'navbar_class': "navbar navbar-inverse",
+#
+#     # Fix navigation bar to top of page?
+#     # Values: "true" (default) or "false"
+#     'navbar_fixed_top': "true",
+#
+#
+#     # Location of link to source.
+#     # Options are "nav" (default), "footer" or anything else to exclude.
+#     #'source_link_position': '',
+#
+#     # Bootswatch (http://bootswatch.com/) theme.
+#     #
+#     # Options are nothing (default) or the name of a valid theme
+#     # such as "amelia" or "cosmo".
+#     #'bootswatch_theme': "journal",
+#     'bootswatch_theme': "flatly",
+#     #'bootswatch_theme': "flatly",
+#
+#
+#     # Choose Bootstrap version.
+#     # Values: "3" (default) or "2" (in quotes)
+#     'bootstrap_version': "3",
+#
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
@@ -159,3 +256,21 @@ texinfo_documents = [
      author, 'giddy', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+#numpydoc option
+numpydoc_show_class_members = True
+numpydoc_class_members_toctree = False
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_use_keyword = True
--- doc/index.rst
+++ doc/index.rst
@@ -9,6 +9,7 @@ Welcome to giddy's documentation!
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+   modules
 
 
 
