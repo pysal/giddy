976d7cd0ad16b0b78b4ad5f70fda0ef962013f0a
Wei Kang <weikang9009@gmail.com>
2018-07-04 18:42:51 -0700

    splot

--- doc/conf.py
+++ doc/conf.py
@@ -35,7 +35,7 @@ import giddy
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_gallery.gen_gallery',
+extensions = [#'sphinx_gallery.gen_gallery',
               'sphinx.ext.autodoc',
               'sphinx.ext.autosummary',
               'sphinx.ext.viewcode',
