32517bbbe08ee69556da6cd74b0a326033aa4348
Wei Kang <weikang9009@gmail.com>
2019-04-08 15:56:04 -0700

    (bug) format readme.rst as long_description for pypi display

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ def setup_package():
           description=DOCLINES[0],
           #long_description="\n".join(DOCLINES[2:]),
           long_description = long_description,
+          long_description_content_type = 'text/x-rst',
           url='https://github.com/pysal/giddy',
           maintainer='Wei Kang',
           maintainer_email='weikang9009@gmail.com',
