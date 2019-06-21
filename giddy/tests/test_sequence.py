ca5d7fe0054800b8534e4c0fe752d92fcdf2ac25
Wei Kang <weikang9009@gmail.com>
2019-06-21 11:24:44 -0700

    add pytests for sequence methods

--- giddy/tests/test_sequence.py
+++ giddy/tests/test_sequence.py
@@ -2,6 +2,8 @@ import unittest
 import numpy as np
 import pytest
 from ..sequence import Sequence
+import libpysal
+import mapclassify as mc
 
 def test_Sequence_unequal():
     '''
@@ -23,10 +25,8 @@ def test_Sequence_unequal():
                             [ 7.,  0.,  3.],
                             [10.,  3.,  0.]])
     assert seqAna.k == 4
-    assert all([a == b for a, b in zip(seqAna.subs_mat.flatten(),
-                                       subs_mat.flatten())])
-    assert all([a == b for a, b in zip(seqAna.seq_dis_mat.flatten(),
-                                       seq_dis_mat.flatten())])
+    np.testing.assert_allclose(seqAna.subs_mat, subs_mat)
+    np.testing.assert_allclose(seqAna.seq_dis_mat, seq_dis_mat)
 
 
     # 1.2 User-defined substitution cost matrix and indel cost
@@ -39,9 +39,7 @@ def test_Sequence_unequal():
     seq_dis_mat = np.array([[0.  , 1.94, 2.46],
                             [1.94, 0.  , 0.76],
                             [2.46, 0.76, 0.  ]])
-    assert all([a == b for a, b in zip(seqAna.seq_dis_mat.flatten(),
-                                       seq_dis_mat.flatten())])
-
+    np.testing.assert_allclose(seqAna.seq_dis_mat, seq_dis_mat)
     # 1.3 Calculating "hamming" distance will fail on unequal sequences
 
     with pytest.raises(ValueError,):
@@ -63,8 +61,7 @@ def test_Sequence_equal():
     seq_dis_mat = np.array([[0., 6., 6.],
                             [6., 0., 1.],
                             [6., 1., 0.]])
-    assert all([a == b for a, b in zip(seqAna.seq_dis_mat.flatten(),
-                                       seq_dis_mat.flatten())])
+    np.testing.assert_allclose(seqAna.seq_dis_mat, seq_dis_mat)
 
     # 2.2 User-defined substitution cost matrix and indel cost (distance
     #     between different types is always 1 and indel cost is 2 or larger) -
@@ -74,8 +71,7 @@ def test_Sequence_equal():
     indel = 2
     seqAna = Sequence([seq1, seq2, seq3], subs_mat=subs_mat, indel=indel)
     seq_dis_mat = np.array([[0., 6., 6.],[6., 0., 1.],[6., 1., 0.]])
-    assert all([a == b for a, b in zip(seqAna.seq_dis_mat.flatten(),
-                                       seq_dis_mat.flatten())])
+    np.testing.assert_allclose(seqAna.seq_dis_mat, seq_dis_mat)
 
     # 2.3 User-defined substitution cost matrix and indel cost (distance
     #     between different types is always 1 and indel cost is 1) - give a
@@ -84,5 +80,55 @@ def test_Sequence_equal():
     indel = 1
     seqAna = Sequence([seq1, seq2, seq3], subs_mat=subs_mat, indel=indel)
     seq_dis_mat = np.array([[0., 5., 5.],[5., 0., 1.],[5., 1., 0.]])
-    assert all([a == b for a, b in zip(seqAna.seq_dis_mat.flatten(),
-                                       seq_dis_mat.flatten())])
+    np.testing.assert_allclose(seqAna.seq_dis_mat, seq_dis_mat)
+
+    f = libpysal.io.open(libpysal.examples.get_path("usjoin.csv"))
+    pci = np.array([f.by_col[str(y)] for y in range(1929, 2010)])
+    q5 = np.array([mc.Quantiles(y, k=5).yb for y in pci]).transpose()
+    seqs = q5[:5, :] #only look at the first 5 sequences
+
+    #"hamming"
+    seq_hamming = Sequence(seqs, dist_type="hamming")
+    seq_dis_mat = np.array([[ 0., 75.,  7., 81., 81.],
+                            [75.,  0., 80., 81., 65.],
+                            [ 7., 80.,  0., 81., 81.],
+                            [81., 81., 81.,  0., 69.],
+                            [81., 65., 81., 69.,  0.]])
+    np.testing.assert_allclose(seq_hamming.seq_dis_mat, seq_dis_mat)
+
+    #"interval"
+    seq_interval = Sequence(seqs, dist_type="interval")
+    seq_dis_mat = np.array([[  0., 123.,   7., 308., 230.],
+                            [123.,   0., 130., 185., 109.],
+                            [  7., 130.,   0., 315., 237.],
+                            [308., 185., 315.,   0.,  90.],
+                            [230., 109., 237.,  90.,   0.]])
+    np.testing.assert_allclose(seq_interval.seq_dis_mat, seq_dis_mat)
+
+    #"arbitrary"
+    seq_arbitrary = Sequence(seqs, dist_type="arbitrary")
+    seq_dis_mat = np.array([[ 0. , 37.5,  3.5, 40.5, 40.5],
+                            [37.5,  0. , 40. , 40.5, 32.5],
+                            [ 3.5, 40. ,  0. , 40.5, 40.5],
+                            [40.5, 40.5, 40.5,  0. , 34.5],
+                            [40.5, 32.5, 40.5, 34.5,  0. ]])
+    np.testing.assert_allclose(seq_arbitrary.seq_dis_mat, seq_dis_mat)
+
+    # "markov"
+    seq_markov = Sequence(seqs, dist_type="markov")
+    seq_dis_mat = np.array([[ 0.        , 72.69026039,  6.44797042, 81.        , 81.        ],
+                            [72.69026039,  0.        , 77.55529756, 80.91834743, 58.93609228],
+                            [ 6.44797042, 77.55529756,  0.        , 81.        , 81.        ],
+                            [81., 80.91834743, 81., 0., 63.14216005],
+                            [81.        , 58.93609228, 81.        , 63.14216005,  0.        ]])
+    np.testing.assert_allclose(seq_markov.seq_dis_mat, seq_dis_mat)
+
+    # "tran"
+    seq_tran = Sequence(seqs, dist_type="tran")
+    seq_dis_mat = np.array([[  0., 220.,  25., 230., 228.],
+                            [220.,   0., 241., 112., 117.],
+                            [ 25., 241.,   0., 241., 241.],
+                            [230., 112., 241.,   0.,  72.],
+                            [228., 117., 241.,  72.,   0.]])
+    np.testing.assert_allclose(seq_tran.seq_dis_mat, seq_dis_mat)
+
