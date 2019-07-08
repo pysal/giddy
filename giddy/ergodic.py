0067bacece6333af56a6ad0dffd72f547ea59e8b
Wei Kang <weikang9009@gmail.com>
2019-07-08 15:32:04 -0700

    (ENH) generalize steady_state and fmpt functions for irreducible and reducible Markov chains.

--- giddy/ergodic.py
+++ giddy/ergodic.py
@@ -1,12 +1,14 @@
 """
-Summary measures for ergodic Markov chains
+Summary measures for ergodic Markov chains.
 """
-__author__ = "Sergio J. Rey <sjsrey@gmail.com>"
+__author__ = "Sergio J. Rey <sjsrey@gmail.com>, Wei Kang <weikang9009@gmail.com>"
 
-__all__ = ['steady_state', 'fmpt', 'var_fmpt']
+__all__ = ['steady_state', 'fmpt', 'var_fmpt',
+           'steady_state_general', 'fmpt_general']
 
 import numpy as np
 import numpy.linalg as la
+import quantecon as qe
 
 
 def steady_state(P):
@@ -58,6 +60,56 @@ def steady_state(P):
     # normalize eigenvector corresponding to the eigenvalue 1
     return row / sum(row)
 
+def steady_state_general(P):
+    """
+    Generalized function for calculating the steady state distribution
+    for a regular or reducible Markov transition matrix P.
+
+    Parameters
+    ----------
+    P        : array
+               (k, k), an ergodic or non-ergodic Markov transition probability
+               matrix.
+
+    Returns
+    -------
+             : array
+               If the Markov chain is irreducible, meaning that
+               there is only one communicating class, there is one unique
+               steady state distribution towards which the system is
+               converging in the long run. Then steady_state_general is the
+               same as steady_state (k, ).
+               If the Markov chain is reducible, meaning that there are
+               more than one communicating class (e.g. `num_classes` = 3),
+               the system could be trapped in any one of these communicating
+               classes. Then, there will be `num_classes` steady state
+               distributions. The returned array will of (num_classes,
+               k) dimension.
+
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> from giddy.ergodic import steady_state_general
+
+    # irreducible Markov chain
+    >>> p = np.array([[.5, .25, .25],[.5,0,.5],[.25,.25,.5]])
+    >>> steady_state_general(p)
+    array([0.4, 0.2, 0.4])
+
+    # reducible Markov chain: two communicating classes
+    >>> p = np.array([[.5, .5, 0],[.2,0.8,0],[0,0,1]])
+    >>> steady_state_general(p)
+    array([[0.28571429, 0.71428571, 0.        ],
+           [0.        , 0.        , 1.        ]])
+    """
+
+    mc = qe.MarkovChain(P)
+    num_classes = mc.num_communication_classes
+    if num_classes == 1:
+        return mc.stationary_distributions[0]
+    else:
+        return mc.stationary_distributions
 
 def fmpt(P):
     """
@@ -116,6 +168,74 @@ def fmpt(P):
     Zdg = np.diag(np.diag(Z))
     M = (I - Z + E * Zdg) * D
     return np.array(M)
+
+def fmpt_general(P):
+    """
+    Generalized function for calculating first mean passage times for an
+    ergodic or non-ergodic transition probability matrix.
+
+    Parameters
+    ----------
+    P       : array
+             (k, k), an ergodic/non-ergodic Markov transition probability
+             matrix.
+
+    Returns
+    -------
+    fmpt_all: array
+              (k, k), elements are the expected value for the number of
+              intervals required for a chain starting in state i to first
+              enter state j. If i=j then this is the recurrence time.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from giddy.ergodic import fmpt_general
+
+    # irreducible Markov chain
+    >>> p = np.array([[.5, .25, .25],[.5,0,.5],[.25,.25,.5]])
+    >>> fm = fmpt_general(p)
+    >>> fm
+    array([[2.5       , 4.        , 3.33333333],
+           [2.66666667, 5.        , 2.66666667],
+           [3.33333333, 4.        , 2.5       ]])
+
+    Thus, if it is raining today in Oz we can expect a nice day to come
+    along in another 4 days, on average, and snow to hit in 3.33 days. We can
+    expect another rainy day in 2.5 days. If it is nice today in Oz, we would
+    experience a change in the weather (either rain or snow) in 2.67 days from
+    today.
+
+    # reducible Markov chain: two communicating classes (this is an
+    artificial example)
+    >>> p = np.array([[.5, .5, 0],[.2,0.8,0],[0,0,1]])
+    >>> fmpt_general(p)
+    array([[3.5, 2. , inf],
+           [5. , 1.4, inf],
+           [inf, inf, 1. ]])
+
+    Thus, if it is raining today in Oz we can expect a nice day to come
+    along in another 2 days, on average, and should not expect snow to hit.
+    We can expect another rainy day in 3.5 days. If it is nice today in Oz,
+    we should expect a rainy day in 5 days.
+
+    """
+
+    P = np.asarray(P)
+    mc = qe.MarkovChain(P)
+    num_classes = mc.num_communication_classes
+    if num_classes == 1:
+        fmpt_all = fmpt(P)
+        return fmpt_all
+    else:
+        i_cclasses = mc.communication_classes_indices
+        fmpt_all = np.full((mc.n, mc.n), np.inf)
+        for cclass in i_cclasses:
+            rows = cclass[:, np.newaxis]
+            p_temp = P[rows, cclass]
+            fmpt_all[rows, cclass] = fmpt(p_temp)
+        return fmpt_all
+
 
 
 def var_fmpt(P):
