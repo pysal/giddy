a7a0ed7fe3120421f5af0900d36f6da17f86cf69
weikang9009 <weikang9009@gmail.com>
2019-07-25 21:28:51 -0700

    (docs) exclude Jupyter backup files for docs building

--- doc/conf.py
+++ doc/conf.py
@@ -92,7 +92,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'tests/*']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'tests/*', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
