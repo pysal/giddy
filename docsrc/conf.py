21dd0f07ca58f59c282ee3d499a35093f81ed816
Wei Kang <weikang9009@gmail.com>
2021-01-19 19:41:41 -0800

    (doc) Configure the extension for sphinxcontrib.bibtex version 2.0.0

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -47,6 +47,10 @@ extensions = [#'sphinx_gallery.gen_gallery',
               'nbsphinx',
               'nbsphinx_link']
 
+# Configure the extension for sphinxcontrib.bibtex: set bibtex_bibfiles to the list of bib files.
+# New in Version 2.0.0 of sphinxcontrib.bibtex
+bibtex_bibfiles = ['_static/references.bib']
+
 mathjax_config = {
     'TeX': {'equationNumbers': {'autoNumber': 'AMS', 'useLabelIds': True}},
 }
