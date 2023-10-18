859cfdaa1b60cbe9e0609e93366e1dd9150403b9
James Gaboardi <jgaboardi@gmail.com>
2023-10-18 18:43:49 -0400

    update __init__ properly

--- giddy/__init__.py
+++ giddy/__init__.py
@@ -4,4 +4,10 @@
 
 """
 
+import contextlib
+from importlib.metadata import PackageNotFoundError, version
+
 from . import directional, ergodic, markov, mobility, rank, sequence, util
+
+with contextlib.suppress(PackageNotFoundError):
+    __version__ = version("giddy")
